ee9e44540feb905cff6237c42e32a763e26ddd1d
madhumenda <madhavimenda56@gmail.com>
2023-04-05 09:19:21 +0000

    cricket team

---
+++ index.js
@@ -0,0 +1,9 @@
+let addDays = require("date-fns/addDays");
+
+function days(d) {
+  let newDate = addDa
+  ys(new Date(2020, 8, 22), d);
+  console.log(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`)
+  );
+}
+days(2);
